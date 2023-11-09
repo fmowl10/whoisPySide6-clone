3adce2c3135a0bd541a9af112bf26b976d3b5da3
minwook <minwook0106@gmail.com>
2023-11-09 16:04:02 +0900

    add query function in async_socket.py

--- whoispyside6_clone/utils/async_socket.py
+++ whoispyside6_clone/utils/async_socket.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from socket import AddressFamily
 
 import aiodns
@@ -14,6 +15,18 @@ class AsyncSocket:
     async def get_host_by_name(self, name, family=AddressFamily.AF_INET):
         return await self.DNS.gethostbyname(name, family)
 
+    async def query(self, name):
+        a = cname = None
+        try:
+            a = await self.DNS.query(name, "A")
+        except aiodns.error.DNSError:
+            logging.debug("A returned answer with no data")
+        try:
+            cname = await self.DNS.query(name, "CNAME")
+        except aiodns.error.DNSError:
+            logging.debug("CNAME returned answer with no data")
+        return {"A": a, "CNAME": cname}
+
 
 if __name__ == '__main__':
     # test code
@@ -27,3 +40,11 @@ if __name__ == '__main__':
     name_test = socket.get_host_by_name("dns.google")
     name_result = test_loop.run_until_complete(name_test)
     print(name_result.addresses)
+
+    record_test = socket.query("google.com")
+    query_result = test_loop.run_until_complete(record_test)
+    print(query_result)
+
+    record_test = socket.query("quantum-classroom.kookmin.ac.kr")
+    query_result = test_loop.run_until_complete(record_test)
+    print(query_result)
